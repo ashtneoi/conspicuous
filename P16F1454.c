f4be3abdfe1b4844aebe3323aa9ab5e38b5d82e0
ashtneoi <ashtneoi@gmail.com>
2015-07-05 16:33:48 -0700

    Finish lexer?

--- P16F1454.c
+++ P16F1454.c
@@ -5,15 +5,19 @@
 #include "cpic.h"
 #include "utils.h"
 
+#include <inttypes.h>
 #include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 
 
 #define CHUNK_LEN 32
+#define EXPECTED(s) do { \
+    if (!optional) fatal(1, "column %u: Expected " s, col); \
+    } while (0)
 
 
-char src_buf[CHUNK_LEN * 2];
+char src_buf[CHUNK_LEN * 2 + 1];
 
 
 enum token_type {
@@ -21,9 +25,7 @@ enum token_type {
     T_PLABEL, // program label
     T_DLABEL, // data label
     T_OPCODE,
-    T_OCTNUM,
-    T_DECNUM,
-    T_HEXNUM,
+    T_NUMBER,
     T_NONE,
 };
 
@@ -31,12 +33,44 @@ enum token_type {
 struct token {
     enum token_type type;
     char* text;
-    int number;
+    uint16_t number;
 };
 
 
+static inline
+void print_token(struct token* token)
+{
+    const char* type;
+    bool is_text;
+
+    if (token->type == T_LABEL) {
+        type = "label";
+        is_text = true;
+    } else if (token->type == T_PLABEL) {
+        type = "program label";
+        is_text = true;
+    } else if (token->type == T_DLABEL) {
+        type = "data label";
+        is_text = true;
+    } else if (token->type == T_OPCODE) {
+        type = "opcode";
+        is_text = true;
+    } else if (token->type == T_NUMBER) {
+        type = "number";
+        is_text = false;
+    } else {
+        fatal(1, "Invalid token");
+    }
+
+    if (is_text)
+        printf("%s: %s\n", type, token->text);
+    else
+        printf("%s: %"PRIX16"\n", type, token->number);
+}
+
+
 struct line {
-    struct token tokens[4];
+    struct token tokens[8];
 };
 
 
@@ -57,13 +91,17 @@ static inline
 ssize_t fill_buffer(const int src, size_t* const pos, size_t* const len,
         const size_t keep)
 {
+    v2("Filling source file buffer (pos = %zd, keep = %zd, len = %zd)",
+        *pos, keep, *len);
     if (*len - keep > CHUNK_LEN)
         fatal(1, "Buffer is already full");
-    v2("Filling source file buffer");
     ssize_t count = bufgrab(src, src_buf, len, CHUNK_LEN, keep);
     if (count < 0)
         fatal_e(1, "Can't read from source file");
-    *pos = *len;
+    *pos = *pos - keep;
+    src_buf[*len] = '\0';
+    v2("src_buf = \"%s\" (pos = %zd, keep = %zd, len = %zd)",
+        src_buf, *pos, keep, *len);
     return count;
 }
 
@@ -83,16 +121,27 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
         S_COMMENT,
     } state = S_PLABEL;
 
+    enum {
+        O_LABEL = 0,
+        O_OCTNUM = 8,
+        O_DECNUM = 10,
+        O_HEXNUM = 16,
+    } opd_type;;
+
     size_t start = *pos;
     size_t col = 1;
 
     bool first = true;
-    while (true) {
+    bool optional = true;
+    bool line_done = false;
+    while (!line_done) {
         char c = src_buf[*pos];
 
         bool change;
         bool advance = true;
         size_t matchlen = *pos - start;
+
+        struct token* token = &line.tokens[token_idx];
 
         if (state == S_PLABEL) {
             change = !(
@@ -108,23 +157,27 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
                     sym[matchlen] = '\0';
                     printf("%s\n", sym);
 
-                    line.tokens[token_idx].type = T_LABEL;
-                    line.tokens[token_idx].text = sym;
+                    token->type = T_LABEL;
+                    token->text = sym;
                     ++token_idx;
 
                     state = S_COLON;
+                    optional = false;
                 } else {
+                    EXPECTED("program label");
                     state = S_OPCODE;
+                    optional = true;
                 }
 
                 advance = false;
             }
         } else if (state == S_COLON) {
             if (c != ':')
-                fatal(1, "column %zu: Missing colon", col);
+                EXPECTED("colon");
 
             change = true;
             state = S_OPCODE;
+            optional = true;
         } else if (state == S_OPCODE) {
             change = !(c >= 'a' && c <= 'z');
 
@@ -135,13 +188,15 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
                     sym[matchlen] = '\0';
                     printf("%s\n", sym);
 
-                    line.tokens[token_idx].type = T_OPCODE;
-                    line.tokens[token_idx].text = sym;
+                    token->type = T_OPCODE;
+                    token->text = sym;
                     ++token_idx;
 
                     state = S_OPERAND;
+                    optional = true;
                 } else {
-                    fatal(1, "column %zu: Missing opcode", col);
+                    EXPECTED("opcode");
+                    line_done = true;
                 }
 
                 advance = false;
@@ -151,81 +206,115 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
                 if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
                         c == '_') {
                     change = false;
-                    line.tokens[token_idx].type = T_LABEL;
+                    opd_type = O_LABEL;
+                    token->type = T_LABEL;
                 } else if (c == '0') {
                     change = false;
+                    token->type = T_NUMBER;
+                    token->number = 0;
                 } else if (c >= '1' && c <= '9') {
                     change = false;
-                    line.tokens[token_idx].type = T_DECNUM;
+                    opd_type = O_DECNUM;
+                    token->type = T_NUMBER;
+                    token->number = c - '0';
                 } else {
-                    change = true;
-                    advance = false;
-                    /*fatal(1, "column %zu: Missing operand", col);*/
+                    EXPECTED("operand");
+                    line_done = true;
                 }
             } else if (matchlen == 1 && src_buf[start] == '0') {
                 if (c >= '0' && c <= '7') {
-                    line.tokens[token_idx].type = T_OCTNUM;
+                    opd_type = O_OCTNUM;
                     change = false;
+                    token->number = c - '0';
                 } else if (c == 'x') {
-                    line.tokens[token_idx].type = T_HEXNUM;
+                    opd_type = O_HEXNUM;
                     change = false;
                 } else {
-                    line.tokens[token_idx].type = T_DECNUM;
+                    opd_type = O_DECNUM;
+                    state = S_COMMA;
+                    optional = true;
                     change = true;
                 }
             } else {
-                enum token_type type = line.tokens[token_idx].type;
-
                 if ((c >= '0' && c <= '7') || c == '_') {
                     change = false;
-                } else if (type == T_HEXNUM && (
-                        (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+                    if (opd_type != O_LABEL)
+                        token->number = token->number * (uint16_t)opd_type +
+                            (c - '0');
+                } else if ((opd_type == O_HEXNUM || opd_type == O_LABEL) &&
+                        (c >= 'A' && c <= 'F')) {
                     change = false;
-                } else if ((type == T_DECNUM || type == T_HEXNUM ||
-                        type == T_LABEL) && (c == 8 || c == 9)) {
+                    if (opd_type != O_LABEL)
+                        token->number = token->number * (uint16_t)opd_type +
+                            (c - 'A');
+                } else if ((opd_type == O_HEXNUM || opd_type == O_LABEL) &&
+                        (c >= 'a' && c <= 'f')) {
                     change = false;
-                } else if (type == T_LABEL && ((c >= 'A' && c <= 'Z') ||
-                        (c >= 'a' && c <= 'z'))) {
+                    if (opd_type != O_LABEL)
+                        token->number = token->number * (uint16_t)opd_type +
+                            (c - 'a');
+                } else if ((opd_type == O_DECNUM || opd_type == O_HEXNUM ||
+                          opd_type == O_LABEL) && (c == 8 || c == 9)) {
+                    change = false;
+                    if (opd_type != O_LABEL)
+                        token->number = token->number * (uint16_t)opd_type +
+                            (c - '0');
+                } else if (opd_type == O_LABEL && ((c >= 'G' && c <= 'Z') ||
+                        (c >= 'g' && c <= 'z'))) {
                     change = false;
                 } else {
                     change = true;
                     advance = false;
 
                     char* sym = malloc(matchlen + 1);
-                    memcpy(sym, &src_buf[start], *pos);
+                    memcpy(sym, &src_buf[start], matchlen);
                     sym[matchlen] = '\0';
                     printf("%s\n", sym);
 
-                    line.tokens[token_idx].text = sym;
+                    token->text = sym;
                     ++token_idx;
 
                     state = S_COMMA;
+                    optional = true;
                 }
             }
         } else if (state == S_COMMA) {
             if (c == ',') {
                 state = S_OPERAND;
+                optional = false;
             } else {
                 state = S_COMMENT;
                 advance = false;
+                optional = true;
             }
 
             change = true;
         } else if (state == S_COMMENT) {
-            if (matchlen == 0) {
-                if (c != ';')
-                    fatal(1, "column %zu: expected semicolon", col);
-            } else if (c == '\n') {
-                break;
+            change = false;
+            if (matchlen == 0 && c != ';') {
+                EXPECTED("semicolon");
+                advance = false;
             }
+        }
+
+        if (src_buf[*pos] == '\n') {
+            line_done = true;
+            advance = true;
+            src_buf[*pos] = ' ';
+            start = *len;
         }
 
         while (advance || src_buf[*pos] == ' ') {
             ++*pos;
             ++col;
             if (*pos >= *len) {
-                if (fill_buffer(src, pos, len, start) == 0)
-                    fatal(1, "Incomplete line");
+                ssize_t count = fill_buffer(src, pos, len, start);
+                if (count == 0) {
+                    if (!optional)
+                        fatal(1, "column %u: Unexpected end of line", col);
+                    line_done = true;
+                    break;
+                }
                 start = 0;
             }
 
@@ -244,7 +333,7 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
 
 bool assemble_16F1454(const int src)
 {
-    size_t pos;
+    size_t pos = 0;
     size_t len = 0;
 
     if (fill_buffer(src, &pos, &len, 0) == 0)
@@ -253,7 +342,13 @@ bool assemble_16F1454(const int src)
     pos = 0;
 
     while (true) {
-        lex_line(src, &pos, &len);
+        v2("Lexing line");
+        struct line line = lex_line(src, &pos, &len);
+        for (unsigned int i = 0; i < lengthof(line.tokens) &&
+                line.tokens[i].type != T_NONE; ++i)
+            print_token(&line.tokens[i]);
+        if (len == 0)
+            break;
     }
 
     return false;
