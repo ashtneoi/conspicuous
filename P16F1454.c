d9d5f59593451452d719956a7f792888c006f9e7
ashtneoi <ashtneoi@gmail.com>
2015-07-08 15:07:21 -0700

    Implement test lexer

--- P16F1454.c
+++ P16F1454.c
@@ -12,12 +12,10 @@
 
 
 #define CHUNK_LEN 32
-#define EXPECTED(s) do { \
-    if (!optional) fatal(1, "column %u: Expected " s, col); \
-    } while (0)
+#define BUFCAP (CHUNK_LEN * 2)
 
 
-char src_buf[CHUNK_LEN * 2 + 1];
+char buf[CHUNK_LEN * 2 + 1];
 
 
 enum token_type {
@@ -88,32 +86,75 @@ struct insn {
 
 
 static inline
-ssize_t fill_buffer(const int src, size_t* const pos, size_t* const len,
-        const size_t keep)
+ssize_t fill_buffer(const int src, size_t* const bufpos, size_t* const buflen,
+        size_t* const keep)
 {
-    v2("Filling source file buffer (pos = %zd, keep = %zd, len = %zd)",
-        *pos, keep, *len);
-    if (*len - keep > CHUNK_LEN)
+    v2("Filling source file buffer (bufpos = %zd, keep = %zd, buflen = %zd)",
+        *bufpos, *keep, *buflen);
+    if (*buflen - *keep > CHUNK_LEN)
         fatal(1, "Buffer is already full");
-    ssize_t count = bufgrab(src, src_buf, len, CHUNK_LEN, keep);
+    ssize_t count = bufgrab(src, buf, buflen, CHUNK_LEN, *keep);
     if (count < 0)
         fatal_e(1, "Can't read from source file");
-    *pos = *pos - keep;
-    src_buf[*len] = '\0';
-    v2("src_buf = \"%s\" (pos = %zd, keep = %zd, len = %zd)",
-        src_buf, *pos, keep, *len);
+    *bufpos = *bufpos - *keep;
+    buf[*buflen] = '\0';
+    *keep = 0;
+    v2("buf = \"%s\" (pos = %zd, keep = %zd, len = %zd)",
+        buf, *bufpos, *keep, *buflen);
     return count;
 }
 
 
 static
-struct line lex_line(const int src, size_t* pos, size_t* len)
+struct line lex_line(const int src, size_t* bufpos, size_t* buflen)
 {
-    (void)src;
-    (void)pos;
-    (void)len;
-    unsigned int token_idx = 0;
     struct line line;
+    unsigned int token_idx = 0;
+    size_t tokstart = *bufpos + 1;
+    char c;
+    size_t toklen;
+    bool first_buf = true;
+
+    do {
+        //
+        // Advance char.
+        //
+
+        ++*bufpos;
+        if (*bufpos >= *buflen) {
+            if (fill_buffer(src, bufpos, buflen, &tokstart) == 0) {
+                if (first_buf)
+                    break;
+                else
+                    fatal(1, "Unexpected end of file");
+            }
+            first_buf = false;
+        }
+        c = buf[*bufpos];
+        toklen = *bufpos - tokstart;
+
+        //
+        // Process char.
+        //
+
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (toklen > 0) {
+                char copy[toklen + 1];
+                memcpy(copy, &buf[tokstart], toklen);
+                copy[toklen] = '\0';
+                v1(copy);
+            }
+        } else {
+            continue;
+        }
+
+        //
+        // Start new token.
+        //
+
+        tokstart = *bufpos + 1;
+    } while (c != '\n');
+
     line.tokens[token_idx].type = T_NONE;
     return line;
 }
@@ -121,21 +162,16 @@ struct line lex_line(const int src, size_t* pos, size_t* len)
 
 bool assemble_16F1454(const int src)
 {
-    size_t pos = 0;
-    size_t len = 0;
-
-    if (fill_buffer(src, &pos, &len, 0) == 0)
-        return true;
-
-    pos = 0;
+    size_t bufpos = 0;
+    size_t buflen = 1;
 
     while (true) {
         v2("Lexing line");
-        struct line line = lex_line(src, &pos, &len);
+        struct line line = lex_line(src, &bufpos, &buflen);
         for (unsigned int i = 0; i < lengthof(line.tokens) &&
                 line.tokens[i].type != T_NONE; ++i)
             print_token(&line.tokens[i]);
-        if (len == 0)
+        if (buflen == 0)
             break;
     }
 
