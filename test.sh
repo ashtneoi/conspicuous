8d14bc661bfe6808a236c11a151828b0ec4f10cf
ashtneoi <ashtneoi@gmail.com>
2015-10-25 12:19:12 -0700

    Fail if either assembler fails; make directives all-caps

--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@ fail() { echo $1; exit 1; }
 
 for name in $(basename -a tests/cpic/*); do
 	echo $name
-	./cpic -p 16F1454 tests/cpic/$name >/tmp/cpic.test.hex
-	gpasm -w 1 -p 16F1454 -a INHX8M -o /tmp/gpasm.test.hex tests/gpasm/$name
+	./cpic -p 16F1454 tests/cpic/$name >/tmp/cpic.test.hex \
+		|| fail "cpic failed"
+	gpasm -w 1 -p 16F1454 -a INHX8M -o /tmp/gpasm.test.hex tests/gpasm/$name \
+		|| fail "gpasm failed"
 	diff /tmp/cpic.test.hex /tmp/gpasm.test.hex || fail "Files differ"
 done
