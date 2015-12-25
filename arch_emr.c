e4a279af7600a3bcbd176e3d2e73a5d9a2c554b1
ashtneoi <ashtneoi@gmail.com>
2015-12-25 00:45:12 -0800

    Implement simple next_token; fix fill_buffer in many ways

--- arch_emr.c
+++ arch_emr.c
@@ -5,12 +5,14 @@
 #include "fail.h"
 #include "utils.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 
-#define CHUNK_LEN 3
+#define CHUNK_LEN 4
+
 
 struct buffer {
     char buf[CHUNK_LEN * 2];
@@ -24,11 +26,11 @@ struct buffer {
 static
 ssize_t fill_buffer(struct buffer* const b)
 {
-    memmove(b->buf + b->tok, b->buf, b->end - b->tok);
+    if (b->end - b->tok >= CHUNK_LEN)
+        fatal(E_RARE, "Buffer is full");
+    memmove(b->buf, b->buf + b->tok, b->end - b->tok);
     b->pos = b->end - b->tok;
     b->tok = 0;
-    if (CHUNK_LEN - b->tok == 0)
-        fatal(E_RARE, "Buffer is full");
     ssize_t count = read(b->src, b->buf + b->pos, CHUNK_LEN - b->pos);
     if (count == -1) {
         fatal_e(E_COMMON, "Can't read from input file");
@@ -42,6 +44,52 @@ ssize_t fill_buffer(struct buffer* const b)
 }
 
 
+struct token {
+    enum token_type {
+        T_CHAR,
+        T_TEXT,
+        T_NUM,
+        T_EOL,
+        T_EOF,
+    } type;
+    char* text;
+    int32_t num;
+};
+
+
+static
+struct token next_token(struct buffer* const b)
+{
+    if (b->pos == b->end && fill_buffer(b) == 0)
+        return (struct token){ .type = T_EOF };
+
+    while (b->buf[b->pos] == ' ' || b->buf[b->pos] == '\t') {
+        b->tok = ++b->pos;
+        if (b->pos == b->end && fill_buffer(b) == 0)
+            return (struct token){ .type = T_EOF };
+    }
+
+    if (b->buf[b->pos] == '\n') {
+        b->tok = ++b->pos;
+        return (struct token){ .type = T_EOL };
+    }
+
+    while (b->buf[b->pos] != ' ' && b->buf[b->pos] != '\t'
+            /*&& b->buf[b->pos] != ',' && b->buf[b->pos] != ';'*/
+            && b->buf[b->pos] != '\n') {
+        ++b->pos;
+        if (b->pos == b->end && fill_buffer(b) == 0)
+            return (struct token){ .type = T_EOF };
+    }
+
+    return (struct token){
+        .type = T_TEXT,
+        .text = b->buf + b->tok,
+        .num = b->pos - b->tok,
+    };
+}
+
+
 void assemble_emr(const int src)
 {
     struct buffer b = {
@@ -51,14 +99,28 @@ void assemble_emr(const int src)
         .pos = 0,
     };
 
+    if (fill_buffer(&b) == 0) {
+        print("EOF\n");
+        return;
+    }
+
     while (true) {
-        if (fill_buffer(&b) == 0) {
-            print("\nEOF\n");
+        /*next_token(&b);*/
+        struct token tkn = next_token(&b);
+
+        if (tkn.type == T_TEXT) {
+            for (size_t i = 0; i < (size_t)tkn.num; ++i)
+                putchar(tkn.text[i]);
+            putchar('\n');
+        } else if (tkn.type == T_EOL) {
+            print("EOL\n");
+        } else if (tkn.type == T_EOF) {
+            print("EOF\n");
             break;
+        } else {
+            print("???\n");
         }
 
-        for (size_t i = b.pos; i < b.end; ++i)
-            putchar(b.buf[i]);
 
         b.tok = b.end;
     }
